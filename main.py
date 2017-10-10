7af171cdb635f499ef22e49377d5bf0d82e800f0
Ryan Yang <javathon@gmail.com>
2017-10-09 20:02:08 -0700

    added note for python 3 compatability

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+#Written for python 2
+#does not work with python 3.6
+#If using python3.6 change import lib to urllib and update bs4 with one compatible with python3
+
 import urllib2
 from bs4 import BeautifulSoup
 
-urlPage = 'http://www.nasdaq.com/symbol/aapl'
+sStockName = raw_input("Enter the name of the stock: ")
+urlPage = "http://www.nasdaq.com/symbol/%s" % sStockName
 
 page = urllib2.urlopen(urlPage)
-
 soup = BeautifulSoup(page, 'html.parser')
 
 priceDivTag = soup.find(id="qwidget_lastsale")
+priceValue = priceDivTag.text.strip("<div class=\"qwidget-dollar\" id=\"qwidget_lastsale\">")
 
-print priceDivTag
-
-#name = name_box.text.strip()
+print (priceValue)
